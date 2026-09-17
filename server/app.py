b71daa6ddeebb8b9b6e2dd0929e9a1197dbb3fcd
Korrnals <153223100+Korrnals@users.noreply.github.com>
2026-09-17 11:58:29 +0300

    chore: version 1.1.4

--- server/app.py
+++ server/app.py
@@ -174,7 +174,7 @@ COLUMN_RU = {
     "resolved": "решено", "done": "готово",
 }
 
-app = FastAPI(title="vesmaro-eyes", version="1.1.3", lifespan=lifespan)
+app = FastAPI(title="vesmaro-eyes", version="1.1.4", lifespan=lifespan)
 
 
 # --------------------------------------------------------------------- models
